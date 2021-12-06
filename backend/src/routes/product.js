a0cd2c02cb0bf6e182b9e784739f69f222ca5d1b
youssef <youssef.mostafa.3112@gmail.com>
2021-12-06 22:39:32 +0200

    get products & get product by Id APIs

--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -3,11 +3,19 @@ const router = express.Router();
 //const { addCAtegory, getCategories } = require("../controller/category");
 const { adminMiddleware,requireSignin } = require("../common-middlewre");
 const Product = require('../models/product');
+const { createProduct , getProducts , getProductById } = require('../controller/product')
+
 
 /*=================== Create New Category Api  ==================*/
 router.post('/product/create', requireSignin , adminMiddleware , createProduct);
 /*=================== Get Categories Api  ==================*/
 //router.get("/product/getcategory", getCategories);
 /*=================== Delete Category Api  ==================*/
+
+/*=================== Get Products Api  ==================*/
+router.get('/product/getproducts',getProducts)
+
+/*=================== Get Single Product by Id Api  ==================*/
+router.get('/product/getproduct/:product_id',getProductById)
 
 module.exports = router;
