eb53cc9344aba32d136d3c2cf48c0ebec842380e
mostafaalgady <mostafaalgady33@gmail.com>
2021-12-06 12:31:11 +0200

    creating react app, homeScreen, productComponent

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="index.html">amazona</a>
+          </div>
+          <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:id" element={<ProductScreen />} ></Route>
+            <Route path="/" element={<HomeScreen />} ></Route>
+          </Routes>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductScreen(props) {
+    return (
+        <div>
+            PRODUCTSCREEN
+        </div>
+    );
+}
+
+export default ProductScreen;
